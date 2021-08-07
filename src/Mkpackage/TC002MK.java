889fab5d9975f452d4dbbe6999150ecd393f6f16
Malika1201 <malika.lrjj@yahoo.com>
2021-08-06 23:59:32 -0500

    package

--- src/Mkpackage/TC002MK.java
+++ src/Mkpackage/TC002MK.java
@@ -4,5 +4,6 @@ public class TC002MK {
     public static void main(String[] args) {
         System.out.println("TC002");
         System.out.println("TC002");
+        System.out.println("TC002");
     }
 }
