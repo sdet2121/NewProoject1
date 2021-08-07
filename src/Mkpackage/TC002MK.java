6987366dfb71326841c262b851d7bcabf059331a
Malika1201 <malika.lrjj@yahoo.com>
2021-08-06 23:59:56 -0500

    commit2

--- src/Mkpackage/TC002MK.java
+++ src/Mkpackage/TC002MK.java
@@ -5,5 +5,6 @@ public class TC002MK {
         System.out.println("TC002");
         System.out.println("TC002");
         System.out.println("TC002");
+        System.out.println("TC002");
     }
 }
